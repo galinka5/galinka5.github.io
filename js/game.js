8d30ba043feb627b3a01a36ab88a2d5a3d5c5193
Halyna <39374722+galinka5@users.noreply.github.com>
2019-10-12 11:40:10 +0200

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,34 @@
+/**
+ * Main class for game Spots
+ *
+ * @author Halyna Kyryliuk
+ * */
+const gameState = {
+   score_p: 0,
+	 score_o: 0
+ };
+
+ //window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio
+
+ const config = {
+   type: Phaser.CANVAS,
+   width: window.innerWidth * window.devicePixelRatio,
+   height: window.innerHeight * window.devicePixelRatio,
+   backgroundColor: "b9eaff",
+	 physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: "game_container",
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    autoRound: false,
+   scene: [SelectScreen, GameScreen]
+ }
+
+ const game = new Phaser.Game(config);
